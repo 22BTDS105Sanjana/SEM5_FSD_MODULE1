1def2e4154e6116f7a9da6582d2870f4689b07c0
22BTDS105Sanjana <srisanjanas2004@gmail.com>
2024-09-04 08:05:40 +0530

    1st module

---
+++ Javascript/function.js
@@ -0,0 +1,19 @@
+function oldVersion()
+{
+    console.log("Hi I am old function")
+}
+oldVersion()
+
+newversion = () =>
+{
+    console.log("Hi I am new function")
+}
+newversion()
+// old function with return 
+function oldVersion2(no1)
+{
+    return no1+10;
+}
+console.log(oldVersion2(30))
+const newversion2 = (no1)=> no1+10
+console.log(newversion2(10))
---
+++ Javascript/spreadoperator.js
@@ -0,0 +1,17 @@
+//spread operator
+const set1 = [10,20,30,40,50]
+const set2 = [...set1,60,70,80,90,100]
+console.log(set2)
+const Student=
+{
+    same:"Raj",
+    sage:20
+
+};
+const SectionD=
+{
+    ...Student,
+    no_Student:71
+
+};
+ console.log(SectionD)
